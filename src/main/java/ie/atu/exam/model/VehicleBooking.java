6eb4f3b2e5bb678ec703511bbce0b3e0f1b99ced
G00407095@atu.ie <G00407095@atu.ie>
2025-11-19 14:14:17 +0000

    created model class for VehicleBooking which includes lombok and validation

--- src/main/java/ie/atu/exam/model/VehicleBooking.java
+++ src/main/java/ie/atu/exam/model/VehicleBooking.java
@@ -1,4 +1,19 @@
 package ie.atu.exam.model;
 
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data@AllArgsConstructor@NoArgsConstructor@Builder
 public class VehicleBooking {
+    @NotBlank
+    private String driverName;
+    @Pattern(regexp = "[0-9]{2,3}-[A-Z]{1,2}-[0-9]{1,6}")
+    private String regNumber;
+    @Email
+    private String email;
+    @Min(1)@Max(8)
+    private int passengers;
 }
