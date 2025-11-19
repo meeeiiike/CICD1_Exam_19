9d0ff077b8f9cafbd182b2a788115a6efc09c0bc
G00407095@atu.ie <G00407095@atu.ie>
2025-11-19 14:02:02 +0000

    init commit

---
+++ src/main/java/ie/atu/exam/Cicd1Exam19Application.java
@@ -0,0 +1,13 @@
+package ie.atu.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicd1Exam19Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cicd1Exam19Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/exam/Cicd1Exam19ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd1Exam19ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
